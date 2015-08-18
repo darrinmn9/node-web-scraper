46523bbba0a8e365f5ff053176cfec0ca5c79efe
Darrin <dnagengast9@gmail.com>
2015-08-17 23:51:57 -0700

    scrapes nhl stats

--- server.js
+++ server.js
@@ -2,44 +2,75 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
-app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+app.get('/', function(req, res) {
+  // this webpage contains all player statistics for 2014-2015 season
+  url = 'http://www.hockey-reference.com/leagues/NHL_2015_skaters.html';
 
-	request(url, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
 
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+      var title, release, rating;
 
-			$('.header').filter(function(){
-		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
+      var json = {
+        'playerData': []
 
-		        json.title = title;
-		        json.release = release;
-	        })
+      };
 
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
+      var headers = [];
 
-	        	json.rating = rating;
-	        })
-		}
+      $('#stats').find('thead').find('tr').last().children().each(function() {
+        headers.push($(this).text().toLowerCase());
+      });
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
+      headers[0] = 'id';
 
-        res.send('Check your console!')
-	})
-})
+      $('#stats').find('tbody').find('tr').filter(function(index, el) {
+        return !$(this).hasClass('thead');
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
-exports = module.exports = app; 	
+      }).each(function(index, el) {
+        var tempObj = {};
+        $(this).children().each(function(index, el) {
+
+          if ($(this).children().length === 0) {
+            tempObj[headers[index]] = $(this).text();
+          } else {
+            tempObj[headers[index]] = $(this).find('a').text();
+          }
+
+        });
+
+        json.playerData.push(tempObj);
+      });
+
+
+
+
+
+
+
+
+      // console.log($('#stats').find($('thead'))));
+      // .each(function() {
+      //   headers.push((this).text());
+      // });
+
+      //console.log(headers);
+
+
+
+    }
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 2), function(err) {
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    });
+
+    res.send('Check your output.json file!');
+  });
+});
+
+app.listen('8081');
+console.log('Scraper listening on port 8081');
+exports = module.exports = app;
